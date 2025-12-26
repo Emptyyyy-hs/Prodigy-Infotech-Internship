de6b2e9ed586be35bc09fbe8d20e52855e50c5e6
Emptyyyy-hs <hiteshsinghh1818@gmail.com>
2025-12-26 14:06:39 +0530

    Add files via upload

---
+++ PRODIGY_WD_02/utils/timeFormatter.ts
@@ -0,0 +1,14 @@
+
+export const formatTime = (ms: number) => {
+  const hours = Math.floor(ms / 3600000);
+  const minutes = Math.floor((ms % 3600000) / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  const centiseconds = Math.floor((ms % 1000) / 10);
+
+  const h = hours.toString().padStart(2, '0');
+  const m = minutes.toString().padStart(2, '0');
+  const s = seconds.toString().padStart(2, '0');
+  const c = centiseconds.toString().padStart(2, '0');
+
+  return { h, m, s, c };
+};
